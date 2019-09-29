82192372315c0dba10107d3d99f154216805d4a4
w2r <wdl1055258582@gmail.com>
2019-09-29 18:22:16 +0800

    update

---
+++ ht.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#判断进程是否存在，如果不存在就启动它
+PIDS=`ps -ef |grep hostloc2tg |grep -v grep | awk '{print $2}'`
+if [ "$PIDS" != "" ]; then
+	echo "myprocess is running!"
+else
+
+	python3 /app/hostloc2tg/hostloc2tg.py &
+fi
+
